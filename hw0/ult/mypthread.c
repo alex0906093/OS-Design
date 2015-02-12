4301efa2e2bbd08ed88974139f46c07b54119500
Apoorv Verma [AP] <avdaredevil@gmail.com>
2015-02-12 15:22:24 -0500

    Added a bunch, removed a bunch

--- hw0/ult/mypthread.c
+++ hw0/ult/mypthread.c
@@ -1,9 +1,11 @@
 /* BY : APoorv || Alex */
 //-----INCLUDES----
+#include <ucontext.h>
 #include <stdio.h>
+#include "queue.h"
 //------MACROS-----
 #define C2LWR(c)   ((c>0x40 && c<0x5b)? c | 0x60:c)
-#define THREAD_THROTTLE 100
+#define STACK_SIZE 16384
 //------COLORS-----
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
@@ -16,9 +18,13 @@
 #define UNDERLINED_POST "\x1b[24m"
 //------AUTOMATE---
 #define ITER_LT(i,j) for (int i=0;i<j;i++)
-//------ERRORS-----
-#define NO_ID -1
-#define INSUFFICIENT_RESOURCES -2
+#define ITER_LT(i,j) for (int i=0;i<j;i++)
+//------STATES-----
+#define T_RUN     1
+#define T_BLOCKED 2
+//------ERR_CD-----
+#define FAIL 1
+#define SUCC 0
 //------DEBUGS-----
 //#define DEBUG //--Code Out @ End
 #ifdef DEBUG
@@ -27,19 +33,30 @@
 #   define D(x) 
 #endif
 //-----------------
-mypthread_t ThreadContainer[THREAD_THROTTLE];
+Queue *Queue = 0;ucontext_t *Main = 0;
 int mypthread_create(mypthread_t *thread, const mypthread_attr_t *attr, void *(*start_routine) (void *), void *ar) {
-    static char FirstCall='y';int id=0;
-    if (FirstCall) {}
-    else {id = GetEmptySlot();}
-    *thread = (mypthread_t){id,2,GetContext()}
-    FirstCall=0;
+    static int id=0;static char FirstCall='y';ucontext_t *ctx = (ucontext_t*)calloc(1,sizeof(ucontext_t));
+    if (FirstCall) {Queue = create_queue();FirstCall=0;GetContext(ctx);Main = ctx;}
+    ctx->uc_stack.ss_sp   = (char*)malloc(STACK_SIZE);
+    ctx->uc_stack.ss_size = STACK_SIZE;
+    ctx->uc_link = TaskScheduler;
+    *thread = (mypthread_t){id++,T_BLOCKED,ctx}
+    enqueue(Queue,thread);
+    makecontext(&ctx,start_routine,ar);
 }
-int GetEmptySlot() {for (int i=0;i<THREAD_THROTTLE;i++) {if (!ThreadContainer[i]) {return i;}}}
+void TaskScheduler() {
+    ucontext_t t;getcontext(t);
+    while (1) {
+        thread=dequeue(Queue);
+        if (thread->state==T_RUN) {break;}
+    }
+    swapcontext(&t,)
+}
 void mypthread_exit(void *retval) {
 
 }
 int mypthread_yield(void) {
+    
 }
 int mypthread_join(mypthread_t thread, void **retval) {
 }
